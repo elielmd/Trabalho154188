217dff7b82a7d382b6857fe68310e228e4baad48
Eliel <eliel.dourado>
2016-06-15 21:17:38 -0300

    Organizando os pacotes do projeto!

---
+++ src/main/java/br/univel/cliente/Cliente.java
@@ -0,0 +1,116 @@
+package br.univel.cliente;
+
+import java.math.BigDecimal;
+
+public class Cliente {
+
+	private int id;
+	private String nome;
+	private String endereco;
+	private String complemento;
+	private String bairro;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+	private String cidade;
+	private String estado;
+	private String cep;
+	private String telefone;
+	private String celular;
+
+	public Cliente(int id, String nome, String endereco, String complemento, String bairro, String cidade,
+			String estado, String cep, String telefone, String celular) {
+		// id, nome, endereço, numero, complemento, bairro, cidade, estado, cep,
+		// telefone, celular
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.endereco = endereco;
+		this.complemento = complemento;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		this.estado = estado;
+		this.cep = cep;
+		this.telefone = telefone;
+		this.celular = celular;
+	}
+
+}
